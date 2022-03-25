7b7afab18f2cb8c9b18d2e5a2d0d97bf62bd0533
aCuriousDev <quentinmarius@gmail.com>
2022-03-25 13:03:27 +0100

    added more styling and results display

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let computerSelection = computerPlay();
-// let playerSelection = prompt('Choose between Rock, Paper and Scissors', 'rock');
+let computerSelection ;
+let playerSelection ;
+let displayPlayerScore = document.getElementById("player-score");
+displayPlayerScore.innerText = 0;
+let displayComputerScore = document.getElementById("computer-score");
+displayComputerScore.innerText = 0;
+let displayResults = document.getElementById("results");
 let computerScore = 0;
 let playerScore = 0;
 let result = "";
@@ -51,25 +56,33 @@ function playRound(playerSelection, computerSelection) {
 // console.log(playRound(playerSelection, computerSelection));
 
 function game() {
+    playerScore = 0;
+    computerScore = 0;
+
     for (let i = 0; i < 5; i++) {
         console.log('Round : ' + (i+1) + (' begin') )
+        computerSelection = computerPlay();
+        playerSelection = prompt('Choose between Rock, Paper and Scissors', 'rock');
         console.log("Computer pick : " + computerSelection);
         console.log("Player pick : " + playerSelection);
         playRound(playerSelection,computerSelection);
+        displayPlayerScore.innerText = playerScore;
+        displayComputerScore.innerText = computerScore;
         console.log(result);
-        console.log('Round : ' + (i+1) + ' is over !')
-        computerSelection = computerPlay();
-        playerSelection = prompt('Choose between Rock, Paper and Scissors', 'rock');
+        console.log('Round : ' + (i+1) + ' is over !');
     }
     console.log('Match is over !');
     console.log('Player Score = ' + playerScore);
     console.log('Computer Score = ' + computerScore);
     if (playerScore > computerScore) {
         console.log('Player Won ! Humanity is in good hands.');
+        displayResults.innerText = 'Player Won ! Humanity is in good hands.';
     } else if (playerScore < computerScore) {
         console.log('You lost ! Beware IA superpower...');
+        displayResults.innerText = 'You lost ! Beware IA superpower...';
     } else {
         console.log("It's a draw... Or is it ? Wait is that even possible ?");
+        displayResults.innerText = "It's a draw... Or is it ? Wait is that even possible ?";
     }
     return 'Player : ' + playerScore + ' | ' + 'Computer : ' + computerScore; 
 }
